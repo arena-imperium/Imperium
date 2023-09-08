57894491cbe8da8a53d9a115304a58b5c169f977
acamill <alexcamill@gmail.com>
2023-09-08 11:47:19 +0800

    Init

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 use bevy::winit::WinitWindows;
 use bevy::DefaultPlugins;
-use bevy_game::GamePlugin; // ToDo: Replace bevy_game with your new crate name.
+use imperium::GamePlugin;
 use std::io::Cursor;
 use winit::window::Icon;
 
@@ -15,7 +15,7 @@ fn main() {
         .insert_resource(ClearColor(Color::rgb(0.4, 0.4, 0.4)))
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
-                title: "Bevy game".to_string(), // ToDo
+                title: "Imperium".to_string(),
                 resolution: (800., 600.).into(),
                 // Bind to canvas included in `index.html`
                 canvas: Some("#bevy".to_owned()),
