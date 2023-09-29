27620ad0fe541430882ced3be526cedb81c4d672
nodespace <nodespace42@gmail.com>
2023-09-28 22:03:53 -0400

    added some comments, and cleaned up code thats not yet needed.

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ enum Scene {
 pub struct GameState {
     /* structures for tracking sengine state should go here*/
     // Note this is different from GameContext in that game context
-    // is a conveniant *reference*, whereas this is where the data is
+    // is a convenient *reference*, whereas this is where the data is
     // actually located.
 
     tasks: TaskPool,
@@ -54,7 +54,8 @@ impl GameState {
     }
 }
 
-/// referenced by doing c.egui, etc.
+/// Used to reference engine internal state mutably
+/// and act as convenient access to common internal state.
 pub struct GameContext<'a, 'b: 'a> {
     // While we could access delta through .engine, it's easier to just expose it once and then
     // benefit all over the codebase.
@@ -78,30 +79,17 @@ fn make_context<'a, 'b: 'a>(
     }
 }
 
-// Setup initial state of the engine, load assets, etc.
+/// Setup initial state of the engine, load assets, etc.
 fn setup(c: &mut GameContext) {
+    // Initializes the task pool
     IoTaskPool::init(||{TaskPoolBuilder::new().build()});
-    // We'll need SFX for this
-    c.engine.load_sound_from_bytes(
-        // Every sound gets a string name later used to reference it.
-        "comfy-flying",
-        include_bytes!(concat!(
-        env!("CARGO_MANIFEST_DIR"),
-        "/assets/audio/flying.ogg"
-        )),
-        StaticSoundSettings::default(),
-    );
-    play_sound("comfy-flying");
 }
 
+/// Called every frame; our main loop.
+///
+/// Drawing and most things are immediate mode; so can be very
+/// quick to setup ui for debugging state.
 fn update(c: &mut GameContext) {
-
-    /*draw_circle(
-        collider.absolute_transform.translation,
-        collider.radius,
-        BLUE.alpha(0.5),
-        4,
-    );*/
 
     draw_text(
         "Welcome to the Imperium galactic Arena!",
@@ -110,7 +98,7 @@ fn update(c: &mut GameContext) {
         TextAlign::Center,
     );
 
-    egui::Window::new("Test Window")
+    egui::Window::new("Dev Test Window")
         .anchor(egui::Align2::CENTER_CENTER, egui::vec2(0.0, 0.0))
         .show(c.egui, |ui| {
             dev_menu(ui, c);
@@ -119,45 +107,3 @@ fn update(c: &mut GameContext) {
     // Handles solana threads and such for us.
     solana_transaction_task_handler(&mut c.engine.commands.borrow_mut(), &mut c.engine.world.borrow_mut());
 }
-
-
-/*fn main() {
-    App::new()
-        .insert_resource(Msaa::Off)
-        .insert_resource(ClearColor(Color::rgb(0.4, 0.4, 0.4)))
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Imperium".to_string(),
-                resolution: (1024., 780.).into(),
-                // Bind to canvas included in `index.html`
-                canvas: Some("#bevy".to_owned()),
-                // Tells wasm not to override default event handling, like F5 and Ctrl+R
-                prevent_default_event_handling: false,
-                ..default()
-            }),
-            ..default()
-        }))
-        .add_plugins(WorldInspectorPlugin::new())
-        .add_plugins(GamePlugin)
-        .add_systems(Startup, set_window_icon)
-        .run();
-}*/
-
-// Sets the icon on windows and X11
-/*fn set_window_icon(
-    windows: NonSend<WinitWindows>,
-    primary_window: Query<Entity, With<PrimaryWindow>>,
-) {
-    let primary_entity = primary_window.single();
-    let primary = windows.get_window(primary_entity).unwrap();
-    let icon_buf = Cursor::new(include_bytes!(
-        "../build/macos/AppIcon.iconset/icon_256x256.png"
-    ));
-    if let Ok(image) = image::load(icon_buf, image::ImageFormat::Png) {
-        let image = image.into_rgba8();
-        let (width, height) = image.dimensions();
-        let rgba = image.into_raw();
-        let icon = Icon::from_rgba(rgba, width, height).unwrap();
-        primary.set_window_icon(Some(icon));
-    };
-}*/
--- src/menu.rs
+++ src/menu.rs
@@ -3,19 +3,6 @@ use {
     comfy::{egui::Ui, *},
 };
 
-pub struct MenuRoot;
-
-#[derive(Clone, Copy, Debug)]
-pub enum MenuButton {
-    Play,
-    InitializeRealm,
-    CreateUserAccount,
-    CreateSpaceship,
-    JoinArenaMatchmakingQueue,
-}
-
-pub struct MenuPlugin;
-
 pub fn dev_menu(ui: &mut Ui, c: &mut GameContext) {
     if ui.button("Init Realm").clicked() {
         c.solana_server
@@ -32,21 +19,4 @@ pub fn dev_menu(ui: &mut Ui, c: &mut GameContext) {
     if ui.button("Join Arena Matchmaking Queue").clicked() {
         //c.solana_server.fire_arena_matchmaking_task(&mut c.engine.commands.borrow_mut());
     }
-    /*match MenuButton::Play {
-        MenuButton::Play => {
-            c.state.set(GameState::Playing);
-        }
-        MenuButton::InitializeRealm => {
-            hologam_server.fire_default_initialize_realm_task(commands.borrow_mut());
-        }
-        MenuButton::CreateUserAccount => {
-            hologam_server.fire_default_create_user_account_task(commands.borrow_mut());
-        }
-        MenuButton::CreateSpaceship => {
-            hologam_server.fire_default_create_spaceship_task(commands.borrow_mut());
-        }
-        MenuButton::JoinArenaMatchmakingQueue => {
-            // hologam_server.fire_arena_matchmaking_task(commands.borrow_mut());
-        }
-    }*/
 }
